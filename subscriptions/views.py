60ae1c4f32fe9f60fdf506592c28ad0d4d3ebaa2
PICHOU Kyâne <kyane@kyane.fr>
2016-12-03 02:40:11 +0100

    Render a basic home page with a template

--- subscriptions/views.py
+++ subscriptions/views.py
@@ -2,7 +2,4 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 def home(request):
-    text = """<h1>Mytube !</h1>
-    <p>Accueil</p>"""
-
-    return HttpResponse(text)
+    return render(request, 'subscriptions/home.html', locals())
