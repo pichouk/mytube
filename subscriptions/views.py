de39ecb3761e7f48dc25531899dc7080f18f1e29
PICHOU Kyâne <kyane@kyane.fr>
2016-12-03 01:25:29 +0100

    Add a test base view

--- subscriptions/views.py
+++ subscriptions/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.http import HttpResponse
+def home(request):
+    text = """<h1>Mytube !</h1>
+    <p>Accueil</p>"""
+
+    return HttpResponse(text)
