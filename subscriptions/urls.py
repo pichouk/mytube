cccf18859a0fa5562775ce7f22737f37a8d5f11a
PICHOU Kyâne <kyane@kyane.fr>
2016-12-03 20:39:43 +0100

    Add a refresh button

--- subscriptions/urls.py
+++ subscriptions/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     url(r'^$', views.home, name='home'),
     url(r'^add$', views.add_channel, name='add_channel'),
+    url(r'^refresh/(?P<id_channel>[\w\W]+)$', views.refresh, name='refresh'),  # Vue d'un article
 ]
--- subscriptions/views.py
+++ subscriptions/views.py
@@ -1,15 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Forms
 from subscriptions.forms import LoginForm, AddChannelForm
 # Model
-from subscriptions.models import Channel
+from subscriptions.models import Channel, Video
 
 # Some python lib
 import requests
 import bs4
+import feedparser
+import time
+from datetime import datetime
 
 def home(request):
+    videos = Video.objects.all()
     return render(request, 'subscriptions/home.html', locals())
 
 def add_channel(request):
@@ -71,3 +75,26 @@ def add_channel(request):
         # Create empty form
         form = AddChannelForm()
         return render(request, 'subscriptions/add_channel.html', locals())
+
+def refresh(request,id_channel):
+    error = False
+    if id_channel == "all":
+        channels = Channel.objects.all()
+    else:
+        channels = Channel.objects.filter(id=id_channel)
+
+    for channel in channels:
+        ### Get the video_id list of this channel
+        videos_id = [v.id for v in Video.objects.filter(channel_id=channel.id)]
+
+        ### Get data from the RSS feed
+        feed = feedparser.parse("https://www.youtube.com/feeds/videos.xml?channel_id="+channel.id)
+        # For each entry
+        for post in feed.entries:
+            video_id = post.get('yt_videoid') ##### TODO ##### Check if exist
+            # If video is already in the database, skip it
+            if video_id in videos_id:
+                continue
+            video = Video(id=video_id,title=post.get('title'),channel_id=channel,date=datetime.fromtimestamp(time.mktime(post.get('published_parsed'))))
+            video.save()
+    return redirect('home')
